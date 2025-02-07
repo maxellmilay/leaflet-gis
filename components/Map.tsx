63ff491e7586dbc22fcb57486dd2ab60f4a7756e
Maxell Milay <milaymaxell@gmail.com>
2025-02-08 06:27:58 +0800

    [Setup][Maxell] initialized leaflet map

---
+++ components/Map.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import React from 'react'
+import { MapContainer, TileLayer, Popup, Marker } from 'react-leaflet'
+import L from 'leaflet'
+
+const icon = L.icon({
+  iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+  shadowSize: [41, 41],
+});
+
+const Map = () => {
+  return (
+    <MapContainer center={[10.327760729473056, 123.92086779061553]} zoom={13} scrollWheelZoom={true}>
+    <TileLayer
+      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />
+    <Marker position={[10.327760729473056, 123.92086779061553]} icon={icon}>
+      <Popup>
+        A pretty CSS3 popup. <br /> Easily customizable.
+      </Popup>
+    </Marker>
+  </MapContainer>
+  )
+}
+
+export default Map
